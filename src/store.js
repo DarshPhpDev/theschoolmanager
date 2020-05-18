93e27295e02f1fb4a70a2f668e14b3ca56d9aecf
AhmedShreef <blogshreef@gmail.com>
2020-05-18 03:27:37 +0300

    add login route

--- src/store.js
+++ src/store.js
@@ -3,25 +3,26 @@ import Vuex from "vuex";
 Vue.use(Vuex);
 
 const state = {
-  sidebarShow: "responsive",
-  sidebarMinimize: false
+    sidebarShow: "responsive",
+    sidebarMinimize: false,
+    logedIn: false
 };
 
 const mutations = {
-  toggleSidebarDesktop(state) {
-    const sidebarOpened = [true, "responsive"].includes(state.sidebarShow);
-    state.sidebarShow = sidebarOpened ? false : "responsive";
-  },
-  toggleSidebarMobile(state) {
-    const sidebarClosed = [false, "responsive"].includes(state.sidebarShow);
-    state.sidebarShow = sidebarClosed ? true : "responsive";
-  },
-  set(state, [variable, value]) {
-    state[variable] = value;
-  }
+    toggleSidebarDesktop(state) {
+        const sidebarOpened = [true, "responsive"].includes(state.sidebarShow);
+        state.sidebarShow = sidebarOpened ? false : "responsive";
+    },
+    toggleSidebarMobile(state) {
+        const sidebarClosed = [false, "responsive"].includes(state.sidebarShow);
+        state.sidebarShow = sidebarClosed ? true : "responsive";
+    },
+    set(state, [variable, value]) {
+        state[variable] = value;
+    }
 };
 
 export default new Vuex.Store({
-  state,
-  mutations
+    state,
+    mutations
 });
